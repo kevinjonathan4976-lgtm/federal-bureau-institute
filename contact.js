da23ed1514c70a93f19094f9a12968560b5aa9df
MaxtecĦ <kevinjonathan4976@gmail.com>
2026-06-24 05:45:40 -0700

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,34 @@
+document.getElementById("contactForm")
+.addEventListener("submit", function(event) {
+
+event.preventDefault();
+
+const status = document.getElementById("status");
+
+status.innerHTML = "Sending message...";
+
+emailjs.sendForm(
+    "FBI_contact_gmail",
+    "template_kcl3en9",
+    this
+)
+
+.then(() => {
+
+    status.innerHTML =
+    "✅ Message sent successfully.";
+
+    this.reset();
+
+})
+
+.catch((error) => {
+
+    status.innerHTML =
+    "❌ Failed to send message.";
+
+    console.error(error);
+
+});
+
+});
