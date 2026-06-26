f76024b428db2d19ac76b1410f0d7f2a4930a918
MaxtecĦ <kevinjonathan4976@gmail.com>
2026-06-26 04:31:18 -0700

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,42 +1,47 @@
 console.log("contact.js loaded");
 
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-
     event.preventDefault();
 
     const status = document.getElementById("status");
-    const form = this;
+
+    const templateParams = {
+        name: this.name.value,
+        email: this.email.value,
+        title: this.title.value
+    };
 
     status.innerHTML = "Sending message...";
 
-    alert("Name: " + form.name.value);
-alert("Email: " + form.email.value);
-alert("Title: " + form.title.value);
-    
-    emailjs.sendForm(
+    emailjs.send(
         "FBI-contact-gmail",
         "template_kcl3en9",
-        form
+        templateParams
     )
 
     .then(() => {
-        // Send auto-reply BEFORE resetting the form
-        return emailjs.sendForm(
+
+        return emailjs.send(
             "FBI-contact-gmail",
             "template_h7mcp39",
-            form
+            templateParams
         );
+
     })
 
     .then(() => {
-        form.reset();
+
         status.innerHTML = "✅ Message sent successfully.";
+        document.getElementById("contactForm").reset();
+
     })
 
     .catch((error) => {
+
         console.error(error);
         alert(JSON.stringify(error));
         status.innerHTML = "❌ Failed to send message.";
+
     });
 
 });
